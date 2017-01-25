099d84bf240b373313aca10f4ba17a87b5e97f49
zhangchao <zhangchaouidesign@163.com>
2017-01-25 21:59:11 +0800

    add readable description for code

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -11,7 +11,16 @@ class App extends Component {
             <div>
                 <h1>
                     <div>
+                        /* 
+                        *FormattedMessage组件中的信息parser后显示以<span>包裹的文本；
+                        *可以通过tagName指定其它标签包裹；
+                        */
                         <FormattedMessage
+                        /*
+                        *以id属性的值"hello"为索引——索引到自定义的映射表'./locale/zh_CN'中去；
+                        *messages['hello']——messages为父组件IntlProvider的props的messages属性；
+                        *若没有上述的返回值，则显示defaultMessage的值"React Intl Translations Example";
+                        */
                             id="hello"
                             defaultMessage="React Intl Translations Example"
                         />
