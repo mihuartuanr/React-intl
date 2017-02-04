33493bafe02f7598363c63245dae1ac9fb20e34f
zhangchao <zhangchaouidesign@163.com>
2017-02-04 21:03:20 +0800

    FormattedMessage add children

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -5,16 +5,22 @@ class App extends Component {
     constructor(props) {
         super(props);
     }
-
     render() {
 
         /* 
-        *由于在以下代码中写注释，webpack打包会报错，所以将注释提出来；
         *FormattedMessage组件中的信息parser后显示以<span>包裹的文本；
         *可以通过tagName指定其它标签包裹；
         *以id属性的值"hello"为索引——索引到自定义的映射表'./locale/zh_CN'中去；
         *messages['hello']——messages为父组件IntlProvider的props的messages属性；
         *若没有上述的返回值，则显示defaultMessage的值"React Intl Translations Example";
+        */
+        /*
+        *FormattedMessage添加子元素或ReactElement；
+        *  <FormattedMessage id="hello">
+        *      {(formattedValue)=>(
+        *          <em>{formattedValue}</em>
+        *      )}
+        *  </FormattedMessage>
         */
         return (
             <div>
@@ -37,6 +43,13 @@ class App extends Component {
                         }}
                     />
                 </h4>
+                <h2>
+                    <FormattedMessage id="hello">
+                        {(formattedValue)=>(
+                            <em>{formattedValue}</em>
+                        )}
+                    </FormattedMessage>
+                </h2>
             </div>
         );
     }
